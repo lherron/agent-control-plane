cad1346c7e5835f58e0738a3b93fbfc7d791fea9
Lance Herron <lherron@gmail.com>
2026-06-25 05:33:44 -0500

    Narrow gateway-discord public barrel

--- packages/gateway-discord/src/index.ts
+++ packages/gateway-discord/src/index.ts
@@ -1,49 +1,8 @@
-export { GatewayDiscordApp, log, startGateway, type GatewayDiscordAppOptions } from './app.js'
-export { BindingIndex, conversationKey, toConversationRefs } from './bindings.js'
+export { GatewayDiscordApp, type GatewayDiscordAppOptions } from './app.js'
 export {
   DEFAULT_BINDINGS_REFRESH_MS,
   DEFAULT_DELIVERY_IDLE_MS,
   DEFAULT_DELIVERY_POLL_MS,
   DEFAULT_MAX_CHARS,
   envNumber,
-  optionalEnv,
 } from './config.js'
-export { createDiscordAttachments, fetchMediaAttachments } from './attachments.js'
-export { renderToDiscord } from './discord-render.js'
-export { classifyDiscordError } from './discord-errors.js'
-export {
-  buildDiscordThreadLaneRef,
-  buildDiscordThreadName,
-  defaultDiscordKeywordDefinitions,
-  parseDiscordKeyword,
-  type DiscordKeywordDefinition,
-  type KeywordRoute,
-  type ParsedDiscordKeyword,
-} from './keywords.js'
-export {
-  extractImagesFromFrame,
-  extractMediaRefsFromFrame,
-  renderActionsToCustomIds,
-  renderFrameToDiscordContent,
-  splitIntoChunks,
-  type ImageAttachment,
-  type MediaRefAttachment,
-  type RenderOptions,
-} from './render.js'
-export {
-  SessionEventsManager,
-  runStateToFrame,
-  type OnRenderCallback,
-  type OnRunQueuedCallback,
-  type RunState,
-} from './session-events-manager.js'
-export type {
-  DiscordInterfaceBinding,
-  GatewaySessionEvent,
-  PermissionAction,
-  RenderAction,
-  RenderBlock,
-  RenderFrame,
-  SessionEventEnvelope,
-  UiHandle,
-} from './types.js'
